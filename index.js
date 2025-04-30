7c1811a74585c5dee2204c274207a3caadce2d59
陶伟 <wei.tao@ninebot.com>
2025-04-30 14:02:35 +0800

    init project

---
+++ index.js
@@ -0,0 +1,128 @@
+import { TurboModuleRegistry, Platform } from 'react-native'
+import AssetSourceResolver from 'react-native/Libraries/Image/AssetSourceResolver'
+//const { Smartassets } = NativeModules
+const Smartassets = TurboModuleRegistry.get('Smartassets')
+let iOSRelateMainBundlePath = ''
+let bundlePath = null
+let _sourceCodeScriptURL
+function getSourceCodeScriptURL() {
+  if (_sourceCodeScriptURL) {
+    return _sourceCodeScriptURL
+  }
+  let sourceCode = global.nativeExtensions && global.nativeExtensions.SourceCode
+  if (!sourceCode) {
+    sourceCode = TurboModuleRegistry && TurboModuleRegistry.get('SourceCode').getConstants()
+  }
+  _sourceCodeScriptURL = sourceCode.scriptURL
+  return _sourceCodeScriptURL
+}
+const defaultMainBundePath = '' //Smartassets.DefaultMainBundlePath;
+var _ = require('lodash')
+var SmartAssets = {
+  initSmartAssets() {
+    var initialize = _.once(this.initSmartAssetsInner)
+    initialize()
+  },
+  initSmartAssetsInner() {
+    let drawablePathInfos = []
+    let drawablePathInfosFromPlatform = []
+    Smartassets.travelDrawable(getSourceCodeScriptURL(), (retArray) => {
+      drawablePathInfos = drawablePathInfos.concat(retArray)
+    })
+    Smartassets.travelDrawable(getPlatformPath(), (retArray) => {
+      drawablePathInfosFromPlatform =
+        drawablePathInfosFromPlatform.concat(retArray)
+    })
+    AssetSourceResolver.prototype.defaultAsset = _.wrap(
+      AssetSourceResolver.prototype.defaultAsset,
+      function (func, ...args) {
+        if (this.isLoadedFromServer()) {
+          return this.assetServerURL()
+        }
+        if (Platform.OS === 'android') {
+          if (this.isLoadedFromFileSystem() || bundlePath != null) {
+            //begin assets ios begin drawable android
+            if (bundlePath != null) {
+              this.jsbundleUrl = bundlePath
+            }
+            let resolvedAssetSource = this.drawableFolderInBundle()
+            let resPath = resolvedAssetSource.uri
+
+            if (drawablePathInfos.includes(resPath)) {
+              //已经在bundle目录中有
+              return resolvedAssetSource
+            }
+
+            let isFileExist = Smartassets.isFileExist(resPath)
+            if (isFileExist === true) {
+              return resolvedAssetSource
+            } else {
+              // 基础包资源里再找一次
+              const lastPath = resPath.split('/res/')[1]
+              if (lastPath) {
+                const fullPath = drawablePathInfosFromPlatform.find((p) =>
+                  p.endsWith(lastPath)
+                )
+                if (fullPath) {
+                  return {
+                    ...resolvedAssetSource,
+                    uri: fullPath
+                  }
+                }
+              }
+              return this.resourceIdentifierWithoutScale()
+            }
+          } else {
+            return this.resourceIdentifierWithoutScale()
+          }
+        } else {
+          if (bundlePath != null) {
+            this.jsbundleUrl = bundlePath
+          }
+          let iOSAsset = this.scaledAssetURLNearBundle()
+          let isFileExist = Smartassets.isFileExist(iOSAsset.uri)
+          if (isFileExist) {
+            return iOSAsset
+          } else {
+            // let oriJsBundleUrl = 'file://'+defaultMainBundePath+'/'+iOSRelateMainBundlePath;
+            iOSAsset.uri = iOSAsset.uri.replace(
+              this.jsbundleUrl,
+              iOSRelateMainBundlePath
+            )
+            return iOSAsset
+          }
+        }
+      }
+    )
+  },
+  setBundlePath(bundlePathNew) {
+    if (!bundlePath) {
+      bundlePath = bundlePathNew
+    }
+  },
+  setiOSRelateMainBundlePath(relatePath) {
+    if (!iOSRelateMainBundlePath) {
+      iOSRelateMainBundlePath = relatePath
+    }
+  }
+}
+
+const platform = 'platform'
+
+function getPlatformPath() {
+  const codeUrl = getSourceCodeScriptURL()
+  if (Platform.OS === 'android') {
+    const p0 = codeUrl.split('/assets/')[0]
+    return (
+      p0.slice(0, p0.lastIndexOf('/')) +
+      '/' +
+      platform +
+      '/assets/' +
+      platform +
+      '.bundle'
+    )
+  }
+  return codeUrl
+}
+
+export { SmartAssets }
